cfb1a336f0e0814bd0f26efd98cb044b24a58a27
felipetodev <fe.ossandon.u@gmail.com>
2022-10-26 12:32:27 -0300

    feat: add coming soon page

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,32 @@ import Head from 'next/head'
 
 const Home = () => {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+    <div className='flex min-h-screen flex-col items-center justify-center'>
       <Head>
-        <title>Create Next App</title>
+        <title>Bloom Bar (Coming soon)</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <h1 className='text-cyan-300'>Hello World</h1>
+      <main className='bg-comming-soon bg-cover bg-center h-screen w-full flex justify-center items-center'>
+        <section className='flex justify-center items-center flex-col gap-24 text-center'>
+          <img src='/bloom-logo.png' alt='Bloom Logo' className='w-11/12 min-w-[800px]' />
+          <div className='flex flex-col justify-center items-center'>
+            <h1 className='text-3xl md:text-6xl text-bloom-orange-100'>Pronto sabrás de nosotros.</h1>
+            <h2 className='text-lg md:text-2xl text-bloom-gray-100 mt-2'>Estamos trabajando en nuestra experiencia digital.</h2>
+          </div>
+          <h2 className='text-lg md:text-2xl text-bloom-gray-100'>
+            Síguenos en{' '}
+            <a
+              href='https://www.instagram.com/bloom_barcl'
+              rel='noopener noreferrer'
+              target='_blank'
+              className='text-bloom-orange-100 underline'
+            >
+              Instagram
+            </a>
+          </h2>
+        </section>
+      </main>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ module.exports = {
     './components/**/*.{js,jsx}'
   ],
   theme: {
-    extend: {}
-  },
-  plugins: []
+    extend: {
+      backgroundImage: {
+        'comming-soon': "url('/coming-soon-bg.jpeg')"
+      },
+      colors: {
+        bloom: {
+          orange: {
+            100: '#FF5C00'
+          },
+          gray: {
+            100: '#E7EBE7',
+            200: '#778078'
+          },
+          dark: {
+            100: '#131B14'
+          },
+          cyan: {
+            100: '#03A2D4'
+          }
+        }
+      }
+    }
+  }
 }
