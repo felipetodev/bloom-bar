50c34d0faebc6381adf0c564e3aeb12288b58cb2
felipetodev <fe.ossandon.u@gmail.com>
2022-11-14 12:08:32 -0300

    feat: add es default lang

--- pages/_document.js
+++ pages/_document.js
@@ -2,11 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document () {
   return (
-    <Html>
+    <Html lang='es'>
       <Head>
         <link rel='stylesheet' href='https://use.typekit.net/qwn6cbc.css' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin='anonymous' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
         <link href='https://fonts.googleapis.com/css2?family=Abel&display=swap' rel='stylesheet' />
       </Head>
       <body>
