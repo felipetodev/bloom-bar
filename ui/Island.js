ad0e699c30f5b0bab62be7cb111f647f4fd1e558
felipetodev <fe.ossandon.u@gmail.com>
2022-11-14 12:55:06 -0300

    fix: island styles

--- ui/Island.js
+++ ui/Island.js
@@ -24,13 +24,19 @@ const Island = () => {
     }
   )
 
+  const triggerStyles = clsx(
+    'IconButton flex justify-center items-center bg-[#CAC2BB] h-[36px] w-[149px] rounded-[3px]', {
+      'ml-2': hasQuerySlug && nextMenu?.color !== 'secondary',
+      'mr-2': hasQuerySlug && nextMenu?.color === 'secondary'
+    })
+
   return (
     <div className='bg-transparent pointer-events-none flex justify-center items-center fixed w-full bottom-0 py-10 bg-island-gradient z-30'>
-      <div className='bg-bloom-black-100 pointer-events-auto flex justify-center items-center rounded-lg border border-solid border-bloom-softGray-100 max-w-max max-h-max min-w-[250px] min-h-[50px]'>
-        <DropdownMenu.Root onOpenChange={(state) => setIsOpen(state)}>
+      <div className='bg-bloom-black-100 pointer-events-auto flex justify-center items-center rounded-[7px] border border-solid border-bloom-softGray-100 max-w-max max-h-max min-w-[250px] min-h-[50px]'>
+        <DropdownMenu.Root open={open} onOpenChange={(state) => setIsOpen(state)}>
           <div className='flex justify-between items-center p-2'>
             {hasQuerySlug && nextMenu?.color !== 'secondary' && (
-              <div className='ml-2 mr-1 rotate-180'>
+              <div className='mr-2 ml-1 rotate-180'>
                 <ArrowNext />
               </div>
             )}
@@ -39,7 +45,7 @@ const Island = () => {
                 <div className={styles}>
                   {nextMenu?.slug === 'nikkei' ? <FlameIcon /> : <WineCupIcon />}
                 </div>
-                <div className='ml-2 mr-2 text-bloom-softGray-100'>
+                <div className='ml-2 text-bloom-softGray-100 self-center w-[70px]'>
                   <p className='text-[8px]'>
                     {hasQuerySlug && nextMenu?.color !== 'secondary' ? 'VOLVER A CARTA' : 'IR A CARTA'}
                   </p>
@@ -50,13 +56,13 @@ const Island = () => {
               </Link>
             )}
             <DropdownMenu.Trigger asChild>
-              <button className='IconButton flex justify-center items-center mr-2 bg-[#CAC2BB] h-[36px] w-[149px] rounded-md' aria-label='Customise options'>
+              <button className={triggerStyles} aria-label='Customise options'>
                 <ArrowIcon isOpen={open} /> CARTA
               </button>
             </DropdownMenu.Trigger>
             {(!hasQuerySlug || nextMenu?.color === 'secondary') && (
               <Link scroll={false} href={`/carta/${nextMenu?.slug}`} className='flex justify-between items-center group'>
-                <div className='mr-2 text-bloom-softGray-100'>
+                <div className='mr-2 text-bloom-softGray-100 self-center w-[70px]'>
                   <p className='text-[8px]'>
                     {hasQuerySlug && nextMenu?.color !== 'secondary' ? 'VOLVER A CARTA' : 'IR A CARTA'}
                   </p>
@@ -74,10 +80,15 @@ const Island = () => {
             )}
           </div>
           <DropdownMenu.Portal>
-            <DropdownMenu.Content align='start' alignOffset={hasQuerySlug && nextMenu?.color !== 'secondary' ? -134 : -4} className='DropdownMenuContent max-h-[400px] sm:max-h-[500px] bg-bloom-black-100 rounded-lg text-bloom-softGray-100 flex flex-col gap-8 border border-solid border-bloom-softGray-100 p-[30px]' sideOffset={5}>
+            <DropdownMenu.Content
+              align='start'
+              alignOffset={hasQuerySlug && nextMenu?.color !== 'secondary' ? -156 : -10}
+              className='DropdownMenuContent max-h-[400px] sm:max-h-[500px] bg-bloom-black-100 rounded-lg text-bloom-softGray-100 flex flex-col gap-8 border border-solid border-bloom-softGray-100 p-[30px]'
+              sideOffset={5}
+            >
               {menuCategoriesCollection?.items?.map(category => (
-                <DropdownMenu.Item key={category.sys.id} className='DropdownMenuItem text-[18px] hover:underline'>
-                  <Link scroll={false} href={`/carta/${slug}/#${category.title}`}>
+                <DropdownMenu.Item key={category.sys.id} onClick={() => setIsOpen(false)} className='DropdownMenuItem text-[18px] hover:underline'>
+                  <Link scroll={false} href={`/carta/${slug}/#${category.title?.replaceAll(' ', '')}`}>
                     {category.title}
                   </Link>
                 </DropdownMenu.Item>
