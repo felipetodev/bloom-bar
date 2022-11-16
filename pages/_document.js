da679542fd475d17e04a22ab1c3ae89851f7753f
felipetodev <fe.ossandon.u@gmail.com>
2022-11-15 21:46:28 -0300

    feat: add new fonts

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,9 @@ export default function Document () {
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
         <link href='https://fonts.googleapis.com/css2?family=Abel&display=swap' rel='stylesheet' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
+        <link href='https://fonts.googleapis.com/css2?family=Marcellus&family=Roboto:wght@100;400;500;700&display=swap' rel='stylesheet' />
       </Head>
       <body>
         <Main />
