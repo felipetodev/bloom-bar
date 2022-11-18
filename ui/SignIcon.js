0a7ba450b7bf9013b69e89a4710422bb983d0d8e
felipetodev <fe.ossandon.u@gmail.com>
2022-11-18 20:54:21 -0300

    feat: add new sign

--- ui/SignIcon.js
+++ ui/SignIcon.js
@@ -2,11 +2,11 @@ import { motion } from 'framer-motion'
 
 const SignIcon = () => {
   return (
-    <svg width='215' height='62' viewBox='0 0 215 62' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <svg width='343' height='6' viewBox='0 0 343 6' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <motion.path
-        d='M2.34375 47.8937C15.2384 45.1306 55.6847 32.495 60.3696 20.8764C68.045 1.84148 -16.6911 50.3498 37.0365 50.3498C90.764 50.3498 145.72 25.1746 134.667 8.90282C123.615 -7.36896 29.0541 40.8324 81.8606 56.4901C134.667 72.1479 194.228 35.3061 206.202 15.6571C218.175 -3.9918 197.912 -3.37777 173.658 19.3413C149.404 42.0604 154.84 62.3234 212.757 37.7622'
+        d='M3.05078 3.39844H340.289'
         stroke='#D16A1B'
-        strokeWidth={3}
+        strokeWidth='5'
         strokeLinecap='round'
         initial={{
           pathLength: 0
