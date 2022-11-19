a19b80c4fff6ff61599ff3f5d650b4dd4eb0a635
felipetodev <fe.ossandon.u@gmail.com>
2022-11-19 15:58:55 -0300

    fix: add revalidate

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -2,9 +2,14 @@ export default async function handler (req, res) {
   const route = req.headers['x-contentful-webhook-name']
 
   if (req.headers['x-secret'] === process.env.REVALIDATE_SECRET) {
-    if (route === '/carta') {
+    if (route === 'allPosts') {
       await res.revalidate('/carta/nikkei')
       await res.revalidate('/carta/drinks')
+      await res.revalidate('/')
+      await res.revalidate('/home')
+      await res.revalidate('/contacto')
+      await res.revalidate('/ubicacion')
+      await res.revalidate('/404')
 
       return res.json({ revalidated: true })
     } else {
