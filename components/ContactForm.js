9540055b73d7d962d12f03d5139dcdb8a1c0d9c3
felipetodev <fe.ossandon.u@gmail.com>
2022-11-19 15:59:12 -0300

    feat: add form submit login

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,36 +1,80 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import Button from '../ui/Button'
-// import FlameIcon from '../ui/FlameIcon'
+import StarIcon from '../ui/StarIcon'
+
+const DEFAULT_STATE = {
+  name: '',
+  email: '',
+  message: ''
+}
+
+const api = {
+  sendForm: async (payload) => {
+    try {
+      const res = await fetch(`https://formsubmit.co/ajax/${process.env.NEXT_PUBLIC_FORM_EMAIL}`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify(payload)
+      })
+      return await res.json()
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
 
 const ContactForm = ({ formPost }) => {
+  const [form, setForm] = useState(DEFAULT_STATE)
+  const [message, setMessage] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (form.name && form.email) {
+      const res = await api.sendForm(form)
+      console.log(res)
+      if (res?.success === 'true') {
+        setMessage(formPost.submitMessage)
+      } else {
+        setMessage(formPost.errorMessage)
+      }
+      setForm(DEFAULT_STATE)
+    }
+  }
+
+  const handleInputChange = ({ target }) => {
+    setForm({ ...form, [target.name]: target.value })
+  }
   return (
     <div data-scroll-section className='grid grid-cols-1 sm:grid-cols-2 bg-bloom-black-100 lg:max-h-[900px]'>
-      <div className='px-5 pt-[200px] sm:pt-0 flex flex-col justify-center items-center sm:px-10 md:px-14 sm:my-28 lg:pb-20 lg:pt-24'>
+      <div className='relative px-5 pt-[200px] sm:pt-0 flex flex-col justify-center items-center sm:px-10 md:px-14 sm:my-28 lg:pb-20 lg:pt-24'>
         <div className='flex flex-col items-center gap-5'>
           <span className='text-bloom-orange-100'>
-            {/* <FlameIcon /> */}
             <Image src='/bien-flama.gif' alt='flame icon' width={16} height={21} />
           </span>
           <h1 className='text-2xl text-bloom-softGray-100 text-center'>
             {formPost?.formTitle}
           </h1>
         </div>
-        <form className='flex flex-col gap-5 pt-10 md:pt-16 w-full'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-5 pt-10 md:pt-16 w-full'>
           <div className='relative z-0 mb-6 w-full group'>
-            <input type='text' name='floating_name' id='floating_name' className='block py-2.5 px-0 w-full text-md text-bloom-softGray-100 bg-transparent border-0 border-b-2 border-bloom-softGray-100 appearance-none focus:outline-none focus:ring-0 focus:border-bloom-orange-100' placeholder=' ' required />
-            <label htmlFor='floating_name' className='peer-focus:font-medium absolute text-md text-bloom-softGray-100 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-bloom-orange-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
+            <input value={form.name} onChange={handleInputChange} type='text' name='name' id='name' className='block py-2.5 px-0 w-full text-md text-bloom-softGray-100 bg-transparent border-0 border-b-2 border-bloom-softGray-100 appearance-none focus:outline-none focus:ring-0 focus:border-bloom-orange-100' placeholder=' ' required />
+            <label htmlFor='name' className='peer-focus:font-medium absolute text-md text-bloom-softGray-100 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-bloom-orange-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
               Nombre
             </label>
           </div>
           <div className='relative z-0 mb-6 w-full group'>
-            <input type='email' name='floating_email' id='floating_email' className='block py-2.5 px-0 w-full text-md text-bloom-softGray-100 bg-transparent border-0 border-b-2 border-bloom-softGray-100 appearance-none focus:outline-none focus:ring-0 focus:border-bloom-orange-100' placeholder=' ' required />
-            <label htmlFor='floating_email' className='peer-focus:font-medium absolute text-md text-bloom-softGray-100 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-bloom-orange-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
+            <input value={form.email} onChange={handleInputChange} type='email' name='email' id='email' className='block py-2.5 px-0 w-full text-md text-bloom-softGray-100 bg-transparent border-0 border-b-2 border-bloom-softGray-100 appearance-none focus:outline-none focus:ring-0 focus:border-bloom-orange-100' placeholder=' ' required />
+            <label htmlFor='email' className='peer-focus:font-medium absolute text-md text-bloom-softGray-100 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-bloom-orange-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
               Email
             </label>
           </div>
           <div className='relative z-0 mb-6 w-full group'>
-            <textarea rows='3' type='email' name='floating_email' id='floating_email' className='block py-2.5 px-0 w-full text-md text-bloom-softGray-100 bg-transparent border-0 border-b-2 border-bloom-softGray-100 appearance-none focus:outline-none focus:ring-0 focus:border-bloom-orange-100' placeholder=' ' required />
-            <label htmlFor='floating_email' className='w-full bg-bloom-black-100 peer-focus:font-medium absolute text-md text-bloom-softGray-100 duration-300 transform -translate-y-6 scale-75 top-3 origin-[0] peer-focus:left-0 peer-focus:text-bloom-orange-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
+            <textarea value={form.message} onChange={handleInputChange} rows='3' name='message' id='message' className='block py-2.5 px-0 w-full text-md text-bloom-softGray-100 bg-transparent border-0 border-b-2 border-bloom-softGray-100 appearance-none focus:outline-none focus:ring-0 focus:border-bloom-orange-100' placeholder=' ' required />
+            <label htmlFor='message' className='w-full bg-bloom-black-100 peer-focus:font-medium absolute text-md text-bloom-softGray-100 duration-300 transform -translate-y-6 scale-75 top-3 origin-[0] peer-focus:left-0 peer-focus:text-bloom-orange-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>
               Mensaje
             </label>
           </div>
@@ -40,6 +84,19 @@ const ContactForm = ({ formPost }) => {
             </Button>
           </div>
         </form>
+        {message && (
+          <div className='absolute flex flex-col justify-center items-center gap-5 top-0 left-0 -bottom-[1px] right-0 bg-bloom-black-100 px-5 sm:px-10 md:px-14'>
+            <StarIcon />
+            <h1
+              className='text-bloom-softGray-100 text-center text-xl'
+              dangerouslySetInnerHTML={{
+                __html: `
+                  ${message}
+                `
+              }}
+            />
+          </div>
+        )}
       </div>
       <div data-scroll data-scroll-speed='1' className='w-full mt-28 max-h-48 sm:min-h-full sm:mt-0'>
         <Image
@@ -47,7 +104,7 @@ const ContactForm = ({ formPost }) => {
           height={720}
           className='object-cover -mt-[3%] min-h-[130%] sm:min-h-[115%] w-full h-full object-bottom sm:object-center'
           alt={formPost?.image?.title}
-          src={formPost.image?.url}
+          src={formPost?.image?.url}
         />
       </div>
     </div>
