c339458c8e571cb8700a1c7a167b10e501738e96
felipetodev <fe.ossandon.u@gmail.com>
2022-11-23 14:08:22 -0300

    fix: remove unused link tags

--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,7 @@ export default function Document () {
         <link rel='stylesheet' href='https://use.typekit.net/qwn6cbc.css' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
-        <link href='https://fonts.googleapis.com/css2?family=Abel&display=swap' rel='stylesheet' />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
-        <link href='https://fonts.googleapis.com/css2?family=Marcellus&family=Roboto:wght@100;400;500;700&display=swap' rel='stylesheet' />
+        <link href='https://fonts.googleapis.com/css2?family=Abel&family=Marcellus&display=swap' rel='stylesheet' />
       </Head>
       <body>
         <Main />
