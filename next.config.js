9c3edf12bb1118640f1972eb86cbc9f6ab1fc0e3
felipetodev <fe.ossandon.u@gmail.com>
2022-11-23 14:52:51 -0300

    feat: add redirect

--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['images.ctfassets.net', 'videos.ctfassets.net']
+  },
+  async redirects () {
+    return [
+      {
+        source: '/carta',
+        destination: '/carta/nikkei',
+        permanent: true
+      }
+    ]
   }
 }
