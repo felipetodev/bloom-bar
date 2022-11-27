01d99cc0e7d11c3428952c686d1dae5b241dd347
felipetodev <fe.ossandon.u@gmail.com>
2022-11-26 23:43:10 -0300

    fix: use tw class

--- ui/ArrowIcon.js
+++ ui/ArrowIcon.js
@@ -1,6 +1,6 @@
 
 const ArrowIcon = ({ isOpen }) => (
-  <svg className='is-active' style={isOpen ? { transform: 'rotate(180deg)' } : {}} focusable='false' width='20' height='20' aria-hidden='true' viewBox='0 0 24 24'>
+  <svg className='transition-all' style={isOpen ? { transform: 'rotate(180deg)' } : {}} focusable='false' width='20' height='20' aria-hidden='true' viewBox='0 0 24 24'>
     <path fill='currentColor' d='M7.41 15.41 12 10.83l4.59 4.58L18 14l-6-6-6 6z' />
   </svg>
 )
