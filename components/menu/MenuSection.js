b38351e63fb55c0feb2f6ab7cbeb074d2e93a836
felipetodev <fe.ossandon.u@gmail.com>
2023-09-06 16:19:17 -0300

    add icon variant color

--- components/menu/MenuSection.js
+++ components/menu/MenuSection.js
@@ -4,43 +4,50 @@ import CardOutlined from './CardOutlined'
 import Dish from './Dish'
 import Heading from './Heading'
 
-const MenuSection = ({ menu, section = [] }) => (
-  <>
-    {section.length > 0 && (
-      <div className='w-full'>
-        <div className='flex flex-col'>
-          {section?.map((category, idx) => (
-            <div key={category.id} className='h-fit'>
-              <Heading title={category.title} subtitle={category.subtitle} variant={menu?.color} />
-              <div className='flex flex-col'>
-                {category.dishes.map((dish) => (
-                  <Dish
-                    key={dish.id}
-                    title={dish.title}
-                    price={dish.price}
-                    hasTitle={dish.uniqueTitle}
-                    description={dish.description}
-                    isVegan={dish.vegan}
-                  />
-                ))}
-              </div>
-              {category.image && (
-                <CardImage cardInfo={category.image} variant={menu?.color} />
-              )}
-              {category.card && (
-                <CardOutlined cardInfo={category.card} />
-              )}
-              {idx !== section?.length - 1 && (
-                <div className='text-bloom-softGray-100 flex justify-center py-[55px] sm:py-[85px]'>
-                  <FlowerIconSpacer />
+const MenuSection = ({ menu, section = [], onSelectImage }) => {
+  return (
+    <>
+      {section.length > 0 && (
+        <div className='w-full'>
+          <div className='flex flex-col'>
+            {section?.map((category, idx) => (
+              <div key={category.id} className='h-fit'>
+                <Heading title={category.title} subtitle={category.subtitle} variant={menu?.color} />
+                <div className='flex flex-col'>
+                  {category.dishes.map((dish) => (
+                    <Dish
+                      key={dish.id}
+                      title={dish.title}
+                      price={dish.price}
+                      hasTitle={dish.uniqueTitle}
+                      description={dish.description}
+                      isVegan={dish.vegan}
+                      isPrimary={menu.color === 'primary'}
+                      image={dish.image}
+                      onSelectImage={() => {
+                        onSelectImage(dish.image)
+                      }}
+                    />
+                  ))}
                 </div>
-              )}
-            </div>
-          ))}
+                {category.image && (
+                  <CardImage cardInfo={category.image} variant={menu?.color} />
+                )}
+                {category.card && (
+                  <CardOutlined cardInfo={category.card} />
+                )}
+                {idx !== section?.length - 1 && (
+                  <div className='text-bloom-softGray-100 flex justify-center py-[55px] sm:py-[85px]'>
+                    <FlowerIconSpacer />
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    )}
-  </>
-)
+      )}
+    </>
+  )
+}
 
 export default MenuSection
